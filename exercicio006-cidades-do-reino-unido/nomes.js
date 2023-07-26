10750899d48af1a670ee1ea76fbf2ff03ca68b7b
Henrique <127993888+heinriqe@users.noreply.github.com>
2023-07-25 21:18:55 -0300

    Finalizando primeiro modulo JS

---
+++ exercicio006-cidades-do-reino-unido/nomes.js
@@ -0,0 +1,13 @@
+var list = document.querySelector('.output ul');
+list.innerHTML = '';
+var cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL'];
+for(var i = 0; i < cities.length; i++) {
+    var input = cities[i];
+    var lower = input.toLowerCase();
+    var priml = lower.slice(0, 1);
+    var capitalized = lower.replace(priml, priml.toUpperCase());
+    var result = capitalized;
+    var listItem = document.createElement('li');
+    listItem.textContent = result;
+    list.appendChild(listItem);
+}
